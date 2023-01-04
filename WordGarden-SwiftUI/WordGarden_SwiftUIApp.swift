5d46ab648d2ad6902d4da7e56cbb8b7cf2afaf51
Jonathan Chasteen <jonathan.chasteen@live.com>
2023-01-04 17:39:39 -0500

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Jonathan Chasteen on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
